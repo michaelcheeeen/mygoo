d0d83da34aa9bc9f6e837725d176b2d5a7e8e43b
michael <343500263@qq.com>
2022-08-17 13:36:34 +0800

    go mod file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/erda/go-web
+
+go 1.19
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World!")
+	//fmt.Fprintf(w, "Hello World!")
+	fmt.Println("Hello Go World!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//func main() {
+//    fmt.Println("Hello Go World!")
+//}
